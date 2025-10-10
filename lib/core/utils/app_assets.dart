2c48f5f103eaff2442296841071ac9db6009056e
mkha2 <mkh515574@gmail.com>
2025-10-10 14:23:48 +0300

    feat: Add assets for registration and goal selection screens
    This commit adds new image assets required for the registration process and the user's goal selection feature.
    
    - **Asset Management**:
        - Added new images for the registration and welcome screens (`re_image.png`, `welcom.png`).
        - Included images for user goal selection: `Improve Shape.png`, `Lean & Tone.png`, and `Lose a Fat.png`.
        - Updated `AppAssets.dart` to include paths for all new images and organized existing asset constants with comments.

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -1,15 +1,39 @@
 class AppAssets {
+
+  //onBoarding images
+
   static const appLogo = "assets/images/app_logo.png";
   static const onBoarding = "assets/images/on_1.png";
   static const onBoarding1 = "assets/images/on_2.png";
   static const onBoarding2 = "assets/images/on_3.png";
   static const onBoarding3 = "assets/images/on_4.png";
+
+
+
+
+  // icons
   static const nameIcon = "assets/icons/user_icon.png";
   static const emailIcon = "assets/icons/email_icon.png";
   static const passwordIcon = "assets/icons/password_icon.png";
   static const googleIcon = "assets/icons/google_icon.png";
   static const facebookIcon = "assets/icons/facebook_icon.png";
   static const loginIcon = "assets/icons/Login.png";
+
+
+
+
+
+  //register images
+
+static const improveShape = "assets/images/Improve Shape.png";
+static const leanTone = "assets/images/Lean & Tone.png";
+static const loseAFat = "assets/images/Lose a Fat.png";
+static const register = "assets/images/re_image.png";
+static const welcome = "assets/images/welcom.png";
+
+
+
+
 
 
 
